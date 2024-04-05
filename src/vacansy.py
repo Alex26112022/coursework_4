d064e36f30d6fb214aa482ccfd3579329a26ff4f
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-04-05 20:28:21 +0400

    Валидация вакансий.

--- src/vacansy.py
+++ src/vacansy.py
@@ -6,15 +6,35 @@ class Vacancy:
         self.vacancy = vacancy
         self.company = company
         self.url = url
-        self. pay = pay
-        self.address = address
-        self.snippet = snippet
-        self.experience = experience
-        self.employment = employment
+        if pay is None:
+            self.pay = 'не указана'
+            self.pay_str = 'не указана'
+        else:
+            self.pay = pay
+            if pay[1] is not None:
+                self.pay_str = f'От {pay[0]} до {pay[1]} {pay[2]}'
+            else:
+                self.pay_str = f'От {pay[0]} {pay[2]}'
+        if address is None:
+            self.address = 'не указан'
+        else:
+            self.address = address
+        if snippet is None:
+            self.snippet = 'не указаны'
+        else:
+            self.snippet = snippet
+        if experience is None:
+            self.experience = 'не указан'
+        else:
+            self.experience = experience
+        if employment is None:
+            self.employment = 'не указан'
+        else:
+            self.employment = employment
 
     def __str__(self):
         return (f'Id: {self.id_}\nВакансия: {self.vacancy}\n'
                 f'Компания: {self.company}\nURL: {self.url}\n'
-                f'Зарплата: {self. pay}\nАдрес: {self.address}\n'
+                f'Зарплата: {self.pay_str}\nАдрес: {self.address}\n'
                 f'Требования: {self.snippet}\nОпыт: {self.experience}\n'
                 f'Тип занятости: {self.employment}\n')
