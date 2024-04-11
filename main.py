4b563c5d42b7e8a3126e21ee50b31800fc932eb1
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-04-11 20:12:19 +0400

    Добавил взаимодействие с фильтрами.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from config import vacancies_json
-from src.func import max_vacancies, sort_of_vacancies
+from src.func import max_vacancies, sort_of_vacancies, experience_input, \
+    employment_input
 from src.json_load import VacancyJson
 from src.sort_vacancies import SortVacancies
 
 
 def main():
-    search_input = input('Введите поисковый запрос: ')
-
+    search_input = input('Введите поисковый запрос: \n')
+    print('Задайте необходимые параметры и нажмите ENTER\n'
+          'Чтобы пропустить параметр оставьте поле пустым и нажмите ENTER')
     max_count_vacancies = max_vacancies()
-
     type_of_sort = sort_of_vacancies()
+    address_input = input('Введите адрес поиска:\n')
+    input_experience = experience_input()
+    input_employment = employment_input()
+    input_snippet = input('Введите через пробел ключевые слова для фильтрации '
+                          'вакансий по требуемым навыкам:\n').split()
 
     # new_search = VacancyJson(search_input)
     # new_search.load_json(vacancies_json)
@@ -19,6 +25,19 @@ def main():
         new_list.sort_pay()
     elif type_of_sort == '2':
         new_list.sort_published()
+
+    if bool(address_input):
+        new_list.filter_address(address_input)
+
+    if input_experience is not None:
+        new_list.filter_experience(input_experience)
+
+    if input_employment is not None:
+        new_list.filter_employment(input_employment)
+
+    if bool(input_snippet):
+        for item in input_snippet:
+            new_list.filter_snippet(item)
 
     if max_count_vacancies is not None:
         my_list = new_list.get_sort_vacancies()[:max_count_vacancies]
--- src/func.py
+++ src/func.py
@@ -1,15 +1,13 @@
 def max_vacancies():
     """ Возвращает топ N вакансий, запрашиваемых у пользователя. """
     max_count_vacancies = input('Введите количество вакансий для вывода '
-                                'в топ N и нажмите ENTER!\n'
-                                'Либо просто нажмите ENTER для вывода '
-                                'всего списка...  ')
+                                'в топ N:\n')
     if not bool(max_count_vacancies):
         max_count_vacancies = None
     else:
         while not max_count_vacancies.isdigit():
             print('Введите положительное целое число!')
-            max_count_vacancies = input('Попробуйте еще раз...')
+            max_count_vacancies = input('Попробуйте еще раз:\n')
             if not bool(max_count_vacancies):
                 max_count_vacancies = None
                 break
@@ -24,15 +22,59 @@ def sort_of_vacancies():
     Запрашивает у пользователя тип сортировки и возвращает
     соответствующий номер.
     """
-    sort_input = input('Введите тип сортировки!\n\t1 - по убыванию '
-                       'минимальной зарплаты\n\t2 - по дате публикации\n'
-                       'Чтобы не применять сортировку просто нажмите ENTER\n')
+    sort_input = input('Введите тип сортировки:\n\t1 - по убыванию '
+                       'минимальной зарплаты\n\t2 - по дате публикации\n')
     if bool(sort_input):
         while sort_input not in ['1', '2'] and bool(sort_input):
-            sort_input = input('Выберите три варианта: "1", "2" или ENTER\n')
+            sort_input = input('Выберите один из двух вариантов: "1", "2"; '
+                               'или пропустите параметр:\n')
 
     return sort_input
 
 
+def experience_input():
+    """
+    Запрашивает у пользователя требуемый опыт работы и возвращает
+    соответствующий ключ.
+    """
+    experience_dict = {'1': 'Нет опыта',
+                       '2': 'От 1 года до 3 лет',
+                       '3': 'От 3 до 6 лет',
+                       '4': 'Более 6 лет'}
+    input_experience = input('Введите требуемый опыт работы:\n'
+                             '\t1 - Нет опыта\n\t2 - От 1 года до 3 лет\n'
+                             '\t3 - От 3 до 6 лет\n\t4 - Более 6 лет\n')
+    if bool(input_experience):
+        while input_experience not in ['1', '2', '3', '4'] and bool(
+                input_experience):
+            input_experience = input('Выберите один из четырех вариантов: '
+                                     '"1", "2", "3", "4"; '
+                                     'или пропустите параметр:\n')
+    return experience_dict.get(input_experience)
+
+
+def employment_input():
+    """
+    Запрашивает у пользователя тип занятости и возвращает
+    соответствующий ключ.
+    """
+    employment_dict = {'1': 'Полная занятость',
+                       '2': 'Частичная занятость',
+                       '3': 'Проектная работа',
+                       '4': 'Волонтерство',
+                       '5': 'Стажировка'}
+    input_employment = input('Введите требуемый тип занятости:\n'
+                             '\t1 - Полная занятость\n\t2 - Частичная занятость\n'
+                             '\t3 - Проектная работа\n\t4 - Волонтерство\n'
+                             '\t5 - Стажировка\n')
+    if bool(input_employment):
+        while input_employment not in ['1', '2', '3', '4', '5'] and bool(
+                input_employment):
+            input_employment = input('Выберите один из пяти вариантов: '
+                                     '"1", "2", "3", "4", "5"; '
+                                     'или пропустите параметр:\n')
+    return employment_dict.get(input_employment)
+
+
 if __name__ == '__main__':
-    print(type(sort_of_vacancies()))
+    print(employment_input())
